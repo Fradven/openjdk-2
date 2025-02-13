0753074158004bdc41bd3fde7b4dcf2df8bb5c07
Fred <frederic.vanovermeire@gmail.com>
2025-02-13 12:21:55 +0100

    use TodoService in Main

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,16 @@
-import entities.Todo;
-import repositories.TodoRepository;
+import services.TodoService;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://database:3306/app", "user", "password");
-            TodoRepository todoRepository = new TodoRepository(connection);
-            List<Todo> todos = todoRepository.findAll();
-
-            Todo todo = new Todo("testing", false, null);
-
-            System.out.println(todoRepository.save(todo));
-
+            TodoService todoService = new TodoService(connection);
+            todoService.showMenu();
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println("Erreur de connexion à la base de données : " + e.getMessage());
         }
     }
 }
